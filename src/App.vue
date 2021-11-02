e3874452db4669b1eae719b39026e65a7120f155
Amrit Kumar Sutar <amrit.sutar@zensar.com>
2021-11-02 15:37:00 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CRC />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CRC from './components/CRC.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CRC
   }
 }
 </script>
@@ -20,7 +19,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
